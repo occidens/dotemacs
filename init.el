ef7b6393d08ce2717eb4fb0a0d7cd20fc4bd6d65
William West <occidens@gmail.com>
2013-11-13 23:26:26 -0500

    Mediawiki-mode

--- init.el
+++ init.el
@@ -3,9 +3,10 @@
 (setq delete-by-moving-to-trash t)
 (setq inhibit-startup-screen t)
 
+;; Set up ELPA
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
+;;(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/") t)
 (package-initialize)
 
 ;; Backup Settings
@@ -19,19 +20,7 @@
    kept-old-versions 2
    version-control t)       ; use versioned backups
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ispell-check-comments nil)
- '(ispell-program-name "C:\\\\Program Files (x86)\\\\Aspell\\\\bin\\aspell.exe"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;MODES
 
 ;;Markdown
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
@@ -48,3 +37,22 @@
 ;;orgmode-mediawiki
 (add-to-list 'load-path "~/.emacs.d/code/gemein/orgmode-mediawiki")
 (require 'ox-mediawiki)
+
+;;mediawiki-mode
+(add-to-list 'load-path "~/.emacs.d/modes/mediawiki-mode")
+(require 'mediawiki)
+
+;;outline mode easy bindings
+;;TODO: also review https://github.com/tj64/outshine
+(add-to-list 'load-path "~/.emacs.d/misc")
+(add-hook 'outline-mode-hook 'my-outline-easy-bindings)
+(add-hook 'outline-minor-mode-hook 'my-outline-easy-bindings)
+(defun my-outline-easy-bindings ()
+       (require 'outline-mode-easy-bindings nil t))
+
+;; Load local stuff
+;;(load "~/.emacs.d/local")
+
+;; Define custom file
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
