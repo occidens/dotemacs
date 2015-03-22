85b7f747469829e47aa44297d1a19ecb9e5cae8e
William West <occidens@gmail.com>
2015-03-21 23:52:22 -0400

    Revise ctrl and hyper key config on darwin

--- systems/darwin.el
+++ systems/darwin.el
@@ -1,9 +1,24 @@
 ;; William West's Emacs Configuration
 
-;; Assign Mac fn key to hyper
-;; See http://irreal.org/blog/?p=1450 and
-;;     http://ergoemacs.org/emacs/emacs_hyper_super_keys.html
-(setq ns-function-modifier 'hyper)
+;; Modifier Key Configuration
+;;
+;; The following configuration enables reasonable interoperability
+;; between the Mac keyboard and Das keyboard
+;;
+;; Caps Lock and Right Control = Control
+;; Control Fn and Left Control = Meta
+;;
+;; To implement, [[https://pqrs.org/osx/karabiner/seil.html.en][Seil]]
+;; is used to map Caps Lock to Right Control
+;;
+;; References
+;; - http://stevelosh.com/blog/2012/10/a-modern-space-cadet/
+;; - http://ergoemacs.org/emacs/emacs_hyper_super_keys.html
+;; - http://irreal.org/blog/?p=145
+;; - http://msol.io/blog/tech/2014/03/10/work-more-efficiently-on-your-mac-for-developers
+(setq ns-function-modifier      'hyper
+      ns-control-modifier       'hyper
+      ns-right-control-modifier 'control)
 
 ;;Spelling
 (setq ispell-program-name "aspell")
