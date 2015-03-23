0313e4c6972a33b7ba9c8d240d74be21a6f33f9a
William West <occidens@gmail.com>
2015-03-23 09:27:00 -0400

    Use new w/ namespace

--- init/init-solarized.el
+++ init/init-solarized.el
@@ -5,30 +5,30 @@
 ;;TODO: examine the difference between sellout's theme and https://github.com/bbatsov/solarized-emacs
 ;;TODO: revise the functions now that there is a single theme with light and dark variants
 
-(defun ww-toggle-theme (theme-1 theme-2)
+(defun w/toggle-theme (theme-1 theme-2)
   "Turn off the current theme and turn on a new one. Order of args does not matter"
   (cond
    ((custom-theme-enabled-p theme-1)
-    (ww-switch-theme theme-2 theme-1))
+    (w/switch-theme theme-2 theme-1))
    ((custom-theme-enabled-p theme-2)
-    (ww-switch-theme theme-1 theme-2))))
+    (w/switch-theme theme-1 theme-2))))
 
-(defun ww-switch-theme (theme-to-enable theme-to-disable)
+(defun w/switch-theme (theme-to-enable theme-to-disable)
   "Enable one theme and disable the other"
   (disable-theme theme-to-disable)
   (when (custom-theme-enabled-p theme-to-enable)
     (enable-theme theme-to-enable))
   (load-theme theme-to-enable t))
 
-(defun ww-toggle-solarized ()
+(defun w/toggle-solarized ()
   "Toggles between solarized light and dark"
   (interactive)
-  (ww-toggle-theme 'solarized-light 'solarized-dark))
+  (w/toggle-theme 'solarized-light 'solarized-dark))
 
-(defun ww-load-solarized ()
+(defun w/load-solarized ()
   "Load Solarized"
   (load-theme 'solarized t))
 
-(add-hook 'after-init-hook 'ww-load-solarized)
+(add-hook 'after-init-hook 'w/load-solarized)
 
 (provide 'init-solarized)
