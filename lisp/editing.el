ddae5f0328d0803d77aa190531098ae529ff0856
William West <occidens@gmail.com>
2015-03-22 21:47:18 -0400

    Add function to unfill a paragraph

--- lisp/editing.el
+++ lisp/editing.el
@@ -43,5 +43,13 @@
     (beginning-of-defun 2)
     (backward-char offset)))
 
+(defun unfill-paragraph (&optional region)
+      "Takes a multi-line paragraph and makes it into a single line of text."
+      (interactive (progn (barf-if-buffer-read-only) '(t)))
+      (let ((fill-column (point-max)))
+        (fill-paragraph nil region)))
+
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
 (provide 'editing)
 ;;; editing.el ends here
