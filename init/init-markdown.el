e42664a2cdf10bd1ee98e7ef59684ac8f1c5c74c
William West <occidens@gmail.com>
2016-07-18 18:55:45 -0400

    Update markdown config

--- init/init-markdown.el
+++ init/init-markdown.el
@@ -30,6 +30,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(markdown-command "kramdown")
+ '(markdown-enable-wiki-links t)
  '(markdown-wiki-link-fontify-missing t)
  '(markdown-wiki-link-search-parent-directories t))
 
